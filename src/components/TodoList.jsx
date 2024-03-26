dde29d6d71ecbc81546d2689aa2809421a7834f1
sergio santana <sergiosantana.m@gmail.com>
2024-03-25 21:33:59 -0400

    todo app

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -48,23 +48,23 @@ export const TodoList = () => {
     <>
       <h1 className="tittle">To Do List</h1>
       <TodoForm onSubmit={addTodo} />
-      {todos.length > 0 ? (
-        <ul className="todo-ul">
-          {todos.map((todo) => (
-            <Todo
-              key={todo.id}
-              id={todo.id}
-              text={todo.text}
-              onRemove={() => removeTask(todo.id)}
-              completed={todo.completed || false}
-              onToggle={() => toggleComplete(todo.id)}
-              onEdit={(newText) => handleEdit(todo.id, newText)}
-            />
-          ))}
-        </ul>
-      ) : (
+        {todos.length > 0 ? (
+          <ul className="todo-ul">
+            {todos.map((todo) => (
+              <Todo
+                key={todo.id}
+                id={todo.id}
+                text={todo.text}
+                onRemove={() => removeTask(todo.id)}
+                completed={todo.completed || false}
+                onToggle={() => toggleComplete(todo.id)}
+                onEdit={(newText) => handleEdit(todo.id, newText)}
+              />
+            ))}
+          </ul>
+        ) : (
         <p className="comt">You have no tasks to do.</p>
-      )}
+        )}
     </>
   );
 };
