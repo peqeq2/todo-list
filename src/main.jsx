a5d965a584c6831b9afb62e438469f8b4b2ef36d
sergio santana <sergiosantana.m@gmail.com>
2024-06-20 21:21:02 -0400

    update todo app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 
 import ReactDOM from 'react-dom/client'
-import { TodoApp } from './TodoApp'
+import { TodoApp } from './components/TodoApp/TodoApp'
+import './index.css';
+
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
+  // <React.StrictMode>
     <TodoApp />
-
+  // </React.StrictMode>
 )
